c5dc53e7e98584c22a52e4f2035c9cb02da0d1fb
Ghidorah <jljohnson1454@gmail.com>
2021-11-11 19:37:43 -0600

    minor change

--- server.js
+++ server.js
@@ -1,49 +1,47 @@
-// // Dependencies
-// // =============================================================
-// const express = require('express');
-// //const exphbs = require('express-handlebars');
-// // Requires the 'express-session' module
-// //const session = require(`express-session`);
-// const sequelize = require('./config/connection');
+// Dependencies
+// =============================================================
+const express = require('express');
+//const exphbs = require('express-handlebars');
+// Requires the 'express-session' module
+//const session = require(`express-session`);
+const sequelize = require('./config/connection');
 
-// // Sets up the Express App
-// // =============================================================
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-// //const hbs = exphbs.create({});
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3001;
+//const hbs = exphbs.create({});
 
-// // Sets Handlebars as the default template engine
-// // app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// // app.set('view engine', 'handlebars');
-// app.use(require('./routes/api/'));
+// Sets Handlebars as the default template engine
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// app.set('view engine', 'handlebars');
+app.use(require('./routes/api/'));
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// // Sets up the sessions with the 'secret', 'resave', 'saveUninitialized' options
-// // app.use(
-// //   session({
-// //     secret: 'This is a major secret!',
-// //     resave: false,
-// //     saveUninitialized: false
-// //   })
-// // );
+// Sets up the sessions with the 'secret', 'resave', 'saveUninitialized' options
+// app.use(
+//   session({
+//     secret: 'This is a major secret!',
+//     resave: false,
+//     saveUninitialized: false
+//   })
+// );
 
 
 
-// // Starts the server to begin listening
-// // =============================================================
-// // app.listen(PORT, () => {
-// //   console.log('App listening on PORT ' + PORT);
-// // });
-
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
+// Starts the server to begin listening
+// =============================================================
+// app.listen(PORT, () => {
+//   console.log('App listening on PORT ' + PORT);
 // });
 
-const express = require('express');
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
 const routes = require('./routes');
-const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -58,3 +56,9 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+
+
+// npm install --save  
+// npm dotenv --save
+// npm install express sequelize mysql2 --save
