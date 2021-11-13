1e299ad8143643578e028d4e6403217d425993f7
Ghidorah <jljohnson1454@gmail.com>
2021-11-13 12:03:03 -0600

    fixed expresshb to previous version

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
 // Dependencies
 // =============================================================
 const express = require('express');
-
-//const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 // Requires the 'express-session' module
 //const session = require(`express-session`);
 const sequelize = require('./config/connection');
-
-// Sets up the Express App
-// =============================================================
-//const hbs = exphbs.create({});
-
-// Sets Handlebars as the default template engine
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
-
-// Sets up the sessions with the 'secret', 'resave', 'saveUninitialized' options
-// app.use(
-//   session({
-//     secret: 'This is a major secret!',
-//     resave: false,
-//     saveUninitialized: false
-//   })
-// );
-
+const hbs = exphbs.create({});
 const routes = require('./routes');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
