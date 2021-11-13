f7aefbc741edb64897a15cb78b4e49249993601c
kboston91 <kourtneygboston@gmail.com>
2021-11-13 11:00:44 -0600

    resolved merge conflict and pulled current develop

---
+++ routes/home-routes.js
@@ -0,0 +1,47 @@
+const { application } = require('express');
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+
+app.get('/', (req,res) => {
+  console.log('hello world');
+  res.render('index', data);
+  
+}
+);
+
+// router.get('/', (req, res) => {
+//   Post.findAll({
+//     attributes: [
+//       'id',
+//       'post_url',
+//       'title',
+//       'created_at',
+//       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+//     ],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+//         include: {
+//           model: User,
+//           attributes: ['username']
+//         }
+//       },
+//       {
+//         model: User,
+//         attributes: ['username']
+//       }
+//     ]
+//   })
+//     .then(dbPostData => {
+//       // pass a single post object into the homepage template
+//       res.render('homepage', dbPostData[0]);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 // Dependencies
 // =============================================================
 const express = require('express');
-
-//const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 // Requires the 'express-session' module
 //const session = require(`express-session`);
 const sequelize = require('./config/connection');
 
 // Sets up the Express App
 // =============================================================
-//const hbs = exphbs.create({});
+const app = express();
+const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
 
 // Sets Handlebars as the default template engine
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(require('./routes/api/'));
 
 //app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
@@ -38,8 +40,6 @@ const sequelize = require('./config/connection');
 
 const routes = require('./routes');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -49,7 +49,7 @@ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening ' + PORT));
 });
 
 
@@ -57,4 +57,5 @@ sequelize.sync({ force: false }).then(() => {
 // npm install --save  
 // npm dotenv --save
 // npm install express sequelize mysql2 --save
-// npm install bcrypt --save
+// npm install express-handlebars
+// npm install bcrypt
