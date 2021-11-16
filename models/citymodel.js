2d9c6911eac02707366055fe3212dd214bc2dc29
kboston91 <kourtneygboston@gmail.com>
2021-11-16 16:36:38 -0600

    added city model info

---
+++ models/citymodel.js
@@ -0,0 +1,31 @@
+const {Model, DataTypes } = require("sequelize" );
+const sequelize = require("../config/connection");
+
+class city extends Model {}
+
+city.init(
+  {
+    id: {
+      type: dataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: dataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: dataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "city",
+  }
+);
+
+module.exports = city;
